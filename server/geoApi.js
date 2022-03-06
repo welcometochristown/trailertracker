db4e197207453c5ce106b212347323fba6113afc
chris <welcometochristown@gmail.com>
2022-03-06 17:22:41 -0500

    small api bug fix

--- server/geoApi.js
+++ server/geoApi.js
@@ -1,17 +1,18 @@
-const express = require('express')
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const { query } = require("./database");
 
 const app = express();
 
 app.use(cors());
-app.get('/location', (req, res) => {
-    query(res);
+app.get("/location", (req, res) => {
+  query(res);
 });
 
 const start = (port) => {
-    app.listen(port, () => {
-        console.log(`Geolocation Api server listening on port ${port}...`);
-    })
-}
+  app.listen(port, () => {
+    console.log(`Geolocation Api server listening on port ${port}...`);
+  });
+};
 
 module.exports = start;
--- server/geoReciever.js
+++ server/geoReciever.js
@@ -1,20 +1,21 @@
-const net = require('net');
-const gprmcParser = require('./parseGPRMC');
-const { insert } = require('./database')
+const net = require("net");
+const gprmcParser = require("./parseGPRMC");
+const { insert } = require("./database");
 
 //create a tcp server
 const server = net.createServer(function (socket) {
-    socket.on('data', data => {
-        var gprmc = gprmcParser(data.toString());
-        insert(gprmc.geo);
-    });
+  socket.on("data", (data) => {
+    var gprmc = gprmcParser(data.toString());
+    console.log(gprmc); //so we can make sure our server is recieving data from host
+    insert(gprmc.geo);
+  });
 });
 
 const start = (port) => {
-    //start geolocation server
-    server.listen(port, () => {
-        console.log(`Geolocation Reciever server listening on port ${port}...`);
-    });
-}
+  //start geolocation server
+  server.listen(port, () => {
+    console.log(`Geolocation Reciever server listening on port ${port}...`);
+  });
+};
 
 module.exports = start;
